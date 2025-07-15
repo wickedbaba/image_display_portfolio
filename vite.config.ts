7976fe1baba591b95841ae16272cbf3052a9f5d0
wickedbaba <aryamansinghranaasr@gmail.com>
2025-07-16 00:20:11 +0530

    deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/image_display_portfolio/', // Add this line for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
